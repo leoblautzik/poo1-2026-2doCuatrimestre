bf9e2a7f14c249b612799800804e254755211b6c
leoblautzik <leoblautzik@gmail.com>
2026-08-14 16:49:54 -0300

    Corregidos nombres de carpetas

--- com1/primeros_pasos/01-03-condicionales/mayor_medio_menor.py
+++ com1/primeros_pasos/01-03-condicionales/mayor_medio_menor.py
@@ -3,23 +3,12 @@ Leer tres valores numéricos enteros,
 indicar cual es el mayor, cuál es el del medio y cuál el menor.
 Considerar que los tres valores son diferentes
 """
-
-
-def main():
-    """
-    Leer tres valores numéricos enteros, indicar cual es el mayor,
-    cuál es el del medio y cuál el menor.
-    Considerar que los tres valores son diferentes.
-    """
-
-    a = int(input("Ingrese un entero: "))
-    b = int(input("Ingrese otro entero: "))
-    c = int(input("Ingrese otro entero mas: "))
+def mayor_medio_menor(a, b ,c):
 
     mayor = a
 
-    if b > a:
-        medio = a
+    if b > mayor:
+        medio = mayor
         mayor = b
 
     else:
@@ -38,6 +27,21 @@ def main():
         menor = c
 
     print("Mayor:", mayor, "Medio:", medio, "Menor:", menor)
+
+
+def main():
+
+    # a = int(input("Ingrese un entero: "))
+    # b = int(input("Ingrese otro entero: "))
+    # c = int(input("Ingrese otro entero mas: "))
+
+    mayor_medio_menor(1,2,3)
+    mayor_medio_menor(1,3,2)
+    mayor_medio_menor(2,1,3)
+    mayor_medio_menor(2,3,1)
+    mayor_medio_menor(3,2,1)
+    mayor_medio_menor(3,1,2)
+    
 
 
 if __name__ == "__main__":
