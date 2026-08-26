22496066faadb8d9747dc7696384a0853fff6eea
leoblautzik <leoblautzik@gmail.com>
2026-08-26 14:26:29 -0300

    Tarea 20-08 ToDo

---
+++ com1/02_encapsulamiento/cubo.py
@@ -0,0 +1,48 @@
+class Cubo:
+    """
+    Implementar la clase Cubo a partir de la siguiente interfaz:
+
+    constructor: inicializa el cubo a partir de la longitud de lado dada
+    obtener_lado
+    cambiar_lado(longitudLado): cambia la longitud de todos los lados del cubo
+    obtener_area_cara: devuelve el área de la superficie de las caras del cubo
+    cambiar_area_cara(areaCara): cambia el área de las caras del cubo
+    obtener_volumen: devuelve el volumen que encierra el cubo
+    cambiar_volumen(volumen): cambia el volumen del cubo
+    """
+
+    def __init__(self) -> None:
+        # ToDo
+        pass
+
+    def obtener_lado(self):
+        # ToDo
+        pass
+
+    def cambiar_lado(self):
+        # ToDo
+        pass
+
+    def obtener_area_cara(self):
+        # ToDo
+        pass
+
+    def cambiar_area_cara(self):
+        # ToDo
+        pass
+
+    def obtener_volumen(self):
+        # ToDo
+        pass
+
+    def cambiar_volumen(self):
+        # ToDo
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
