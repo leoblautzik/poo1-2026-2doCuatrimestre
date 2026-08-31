8abc9e63faa6dddd002c8a6337271f37ed0e2348
leoblautzik <leoblautzik@gmail.com>
2026-08-30 22:36:14 -0300

    cerradura resuelto con test

--- com1/02_encapsulamiento/src/cerradura.py
+++ com1/02_encapsulamiento/src/cerradura.py
@@ -18,8 +18,11 @@ class Cerradura:
     def __init__(self, clave, cfcqlb) -> None:
         self.__clave = clave
         self.__cfcqlb = cfcqlb
-        self.__abierta = True
+        self.__abierta = False
         self.__bloqueada = False
+        self.__aperturas_exitosas = 0
+        self.__aperturas_fallidas = 0
+        self.__fallos_consecutivos = 0
 
     def esta_abierta(self) -> bool:
         return self.__abierta
@@ -30,13 +33,44 @@ class Cerradura:
     def esta_bloqueada(self) -> bool:
         return self.__bloqueada
 
-    def abrir(self, clave):
-        pass
+    def fue_bloqueada(self) -> bool:
+        return self.__bloqueada
+
+    def contar_aperturas_exitosas(self) -> int:
+        return self.__aperturas_exitosas
+
+    def contar_aperturas_fallidas(self) -> int:
+        return self.__aperturas_fallidas
+
+    def abrir(self, clave) -> bool:
+        if self.fue_bloqueada():
+            raise RuntimeError("La cerradura fue bloqueada")
+        if self.esta_abierta():
+            raise RuntimeError("La cerradura esta abierta")
+
+        if clave == self.__clave:
+            self.__aperturas_exitosas += 1
+            self.__abierta = True
+            self.__fallos_consecutivos = 0
+            return True
+        else:
+            self.__aperturas_fallidas += 1
+            self.__fallos_consecutivos += 1
+            if self.__fallos_consecutivos == self.__cfcqlb:
+                self.__bloqueada = True
+            return False
+
+    def cerrar(self) -> None:
+        if self.fue_bloqueada():
+            raise RuntimeError("La cerradura fue bloqueada")
+        if self.esta_cerrada():
+            raise RuntimeError("La cerradura esta cerrada")
+        self.__abierta = False
 
 
 def main():
     trabex = Cerradura(1234, 3)
-    print(trabex.esta_abierta())
+    print(trabex.esta_cerrada())
 
 
 if __name__ == "__main__":
