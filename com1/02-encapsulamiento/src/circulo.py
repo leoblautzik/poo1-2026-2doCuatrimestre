887460c5f3ce024c4ba83551a4652ab1b78ac3bd
leoblautzik <leoblautzik@gmail.com>
2026-09-06 14:25:46 -0300

    Encapsulamiento completo

--- com1/02-encapsulamiento/src/circulo.py
+++ com1/02-encapsulamiento/src/circulo.py
@@ -1,9 +1,14 @@
+from __future__ import annotations
+
 import math
+
+from src.punto import Punto
 
 
 class Circulo:
-    def __init__(self, radio):
+    def __init__(self, centro: Punto, radio: float):
         self.radio = radio
+        self.__centro = centro
 
     @property
     def radio(self):
@@ -39,6 +44,14 @@ class Circulo:
     def area(self, nuevo_area):
         self.radio = math.sqrt(nuevo_area / math.pi)
 
+    @property
+    def centro(self) -> Punto:
+        return self.__centro
+
+    @centro.setter
+    def centro(self, nuevo_centro: Punto) -> None:
+        self.__centro = nuevo_centro
+
     def __str__(self):
         s = "Soy un círculo felíz\n"
         s += f"radio: {self.__radio}\n"
@@ -47,16 +60,22 @@ class Circulo:
         s += f"area: {self.area}"
         return s
 
+    def intersecta_con(self, otro_circulo: Circulo) -> bool:
+        return (
+            self.centro.distancia(otro_circulo.centro)
+            <= self.radio + otro_circulo.radio
+        )
+
 
 def main():
-    circulito = Circulo(4)
+    circulito = Circulo(Punto(1, 1), 4)
     print(circulito)
     circulito.radio = 5
     print(circulito)
     circulito.area = 36
     print(circulito)
 
-    c1 = Circulo(1)
+    c1 = Circulo(Punto(1, 1), 1)
     print(c1)
 
 
